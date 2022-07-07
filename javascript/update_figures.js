a4d212bd65f46b8e99e5e7ca30ea40f1000f4580
Alfred Millett-Sikking <amsikking@gmail.com>
2022-07-07 10:47:40 -0700

    Update update_figures.js

--- javascript/update_figures.js
+++ javascript/update_figures.js
@@ -19,3 +19,30 @@ function update_figure_4() {
   var img = document.getElementById("fig_4_png");
   img.src = filename;
 }
+
+function update_figure_A2() {
+  var config = document.getElementById("figure_A2_choice_configuration").value;
+  var figure = document.getElementById("figure_A2_choice_figure").value;
+  if ((config === "_config_8")){
+  var filename = "./figures/zoom_lens/optics/performance/" + figure + config + ".png";}
+  if ((config === "_config_1")){
+  var filename = "./figures/zoom_lens/optics/performance/" + figure + config + ".png";}
+  var img = document.getElementById("fig_A2_png");
+  img.src = filename;
+}
+
+function update_figure_A4() {
+  var number = document.getElementById("figure_A4_choice_sheet").value;
+  if ((number === "1")){
+  var filename = "./figures/zoom_lens/mechanics/images/Zoom_lens_132.5-150mm_sheet_" + number + ".png";}
+  if ((number === "2")){
+  var filename = "./figures/zoom_lens/mechanics/images/Zoom_lens_132.5-150mm_sheet_" + number + ".png";}
+  if ((number === "3")){
+  var filename = "./figures/zoom_lens/mechanics/images/Zoom_lens_132.5-150mm_sheet_" + number + ".png";}
+  if ((number === "4")){
+  var filename = "./figures/zoom_lens/mechanics/images/Zoom_lens_132.5-150mm_sheet_" + number + ".png";}
+  if ((number === "5")){
+  var filename = "./figures/zoom_lens/mechanics/images/Zoom_lens_132.5-150mm_sheet_" + number + ".png";}
+  var img = document.getElementById("fig_A4_png");
+  img.src = filename;
+}
