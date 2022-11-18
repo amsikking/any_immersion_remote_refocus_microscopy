c95e757e17207e55f516c4ad67571a0fab88eacd
Alfred Millett-Sikking <amsikking@gmail.com>
2022-11-18 15:26:15 -0800

    Update update_figures.js

--- javascript/update_figures.js
+++ javascript/update_figures.js
@@ -1,22 +1,22 @@
-function update_figure_3() {
-  var objectives = document.getElementById("figure_3_choice_objectives").value;
-  var scale = document.getElementById("figure_3_choice_scale").value;
+function update_figure_5() {
+  var objectives = document.getElementById("figure_5_choice_objectives").value;
+  var scale = document.getElementById("figure_5_choice_scale").value;
   if ((scale === "")){
   var filename = "./figures/sf_and_rr_range/sf_range" + objectives + scale + ".png";}
   if ((scale === "_log")){
   var filename = "./figures/sf_and_rr_range/sf_range" + objectives + scale + ".png";}
-  var img = document.getElementById("fig_3_png");
+  var img = document.getElementById("fig_5_png");
   img.src = filename;
 }
 
-function update_figure_4() {
-  var objectives = document.getElementById("figure_4_choice_objectives").value;
-  var scale = document.getElementById("figure_4_choice_scale").value;
+function update_figure_6() {
+  var objectives = document.getElementById("figure_6_choice_objectives").value;
+  var scale = document.getElementById("figure_6_choice_scale").value;
   if ((scale === "")){
   var filename = "./figures/sf_and_rr_range/sf_and_rr_range" + objectives + scale + ".png";}
   if ((scale === "_log")){
   var filename = "./figures/sf_and_rr_range/sf_and_rr_range" + objectives + scale + ".png";}
-  var img = document.getElementById("fig_4_png");
+  var img = document.getElementById("fig_6_png");
   img.src = filename;
 }
 
