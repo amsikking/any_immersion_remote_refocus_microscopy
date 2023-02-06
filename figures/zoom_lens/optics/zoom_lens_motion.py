4c70fa067d81650135a22777676acf62f800a5e6
Alfred Millett-Sikking <amsikking@gmail.com>
2023-02-06 14:44:05 -0800

    Add files via upload
    zoom lens optics update

--- figures/zoom_lens/optics/zoom_lens_motion.py
+++ figures/zoom_lens/optics/zoom_lens_motion.py
@@ -2,29 +2,42 @@ import numpy as np
 
 # Zoom_lens_132.5-150mm_motion.ods trendline polynomials
 # Data from Zemax
-def negative_lens_motion(f_mm):
-    motion_mm = (- 0.012921858472509 * f_mm**2
-                 + 6.46780791066724 * f_mm
-                 - 630.281528419234)
-    return motion_mm
 
-def positive_group_motion(f_mm):
-    motion_mm = (+ 0.005518525287293 * f_mm**2
-                 + 0.885914869404723 * f_mm
-                 - 214.401055085496)
-    return motion_mm
+def focal_length_to_lens_motion(f_mm, verbose=True):
+    stage1_mm = (-     0.000103278288*f_mm**4
+                 +     0.059929457484*f_mm**3
+                 -    13.033982374525*f_mm**2
+                 +  1260.150733009060*f_mm
+                 - 45717.573446038600)
+    stage2_mm = (-     0.000083979119*f_mm**4
+                 +     0.049656589716*f_mm**3
+                 -    10.961078884880*f_mm**2
+                 +  1074.586675413890*f_mm
+                 - 39574.398463973600)
+    stage3_mm = (+    0.000018343468*f_mm**4
+                 -    0.009692931167*f_mm**3
+                 +    1.950506313956*f_mm**2
+                 -  175.313955214666*f_mm
+                 + 5879.390039196680)
+    if verbose:
+        print('\nf_mm = %0.2f'%f_mm)
+        print('stage1_mm = %0.2f'%stage1_mm)
+        print('stage2_mm = %0.2f'%stage2_mm)
+        print('stage3_mm = %0.2f'%stage3_mm)
+    return stage1_mm, stage2_mm, stage3_mm
 
 # Zemax data:
 Zemax_f_mm = (132.5, 135.0, 137.5, 140.0, 142.5, 145.0, 147.5, 150.0)
-Zemax_N_mm = (0.00, 7.27, 14.58, 21.87, 29.05, 36.03, 42.71, 48.99)
-Zemax_P_mm = (0.00, 5.68, 11.61, 17.72, 23.96, 30.23, 36.44, 42.51)
+Zemax_stage1_mm = (0.00, 3.46, 6.14, 8.53, 10.73, 13.06, 15.32, 17.73)
+Zemax_stage2_mm = (0.00, 9.27, 18.07, 26.88, 35.80, 45.36, 55.23, 65.90)
+Zemax_stage3_mm = (0.00, 4.53, 9.45, 14.79, 20.55, 26.92, 33.80, 41.43)
 
 # Input:
 f_mm = np.linspace(132.5, 150, 1000)
 
 # Output:
-N_zoom_mm = negative_lens_motion(f_mm)
-P_zoom_mm = positive_group_motion(f_mm)
+stage1_mm, stage2_mm, stage3_mm = focal_length_to_lens_motion(
+    f_mm, verbose=False)
 
 # Plot:
 import matplotlib.pyplot as plt
@@ -34,15 +47,19 @@ ax.set_title('Zoom lens (constant track length)')
 ax.set_ylabel('Motion (mm)')
 ax.set_xlabel('Focal length (mm)')
 # data:
-ax.plot(Zemax_f_mm, Zemax_N_mm, 'b', linestyle='', marker='o',
-        label='Zemax negative lens data')
-ax.plot(Zemax_f_mm, Zemax_P_mm, 'r', linestyle='', marker='^',
-        label='Zemax positive group data')
+ax.plot(Zemax_f_mm, Zemax_stage1_mm, 'b', linestyle='', marker='o',
+        label='stage1: zemax data')
+ax.plot(Zemax_f_mm, Zemax_stage2_mm, 'r', linestyle='', marker='^',
+        label='stage2: zemax data')
+ax.plot(Zemax_f_mm, Zemax_stage3_mm, 'y', linestyle='', marker='^',
+        label='stage3: zemax data')
+
 # curves:
-ax.plot(f_mm, N_zoom_mm, 'b', linestyle='-', label='negative lens function')
-ax.plot(f_mm, P_zoom_mm, 'r', linestyle='--', label='positive group function')
+ax.plot(f_mm, stage1_mm, 'b', linestyle='-',  label='stage1: polynomial')
+ax.plot(f_mm, stage2_mm, 'r', linestyle='--', label='stage2: polynomial')
+ax.plot(f_mm, stage3_mm, 'y', linestyle='-.', label='stage3: polynomial')
 # configure:
-ax.legend(loc="lower right", framealpha=1)
+ax.legend(loc="upper left", framealpha=1)
 fig.savefig('zoom_lens_motion.png', dpi=150)
 plt.show()
 plt.close()
