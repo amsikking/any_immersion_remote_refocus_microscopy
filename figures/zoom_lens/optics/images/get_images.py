82fbb4baa802835a0421851a6899c3d3c9eec407
Alfred Millett-Sikking <amsikking@gmail.com>
2023-03-23 16:15:32 -0700

    Update get_images.py
    Updating cropping to match 'reversed' animation

--- figures/zoom_lens/optics/images/get_images.py
+++ figures/zoom_lens/optics/images/get_images.py
@@ -2,13 +2,23 @@ import os
 import imageio
 
 d = os.path.dirname(os.getcwd()) + '\\animation_images\\'
-filenames = ('0.png','1.png','2.png','3.png','4.png','5.png','6.png','7.png')
+filenames = ('0.png',
+             '1.png',
+             '2.png',
+             '3.png',
+             '4.png',
+             '5.png',
+             '6.png',
+             '7.png')
 
+ud_crop_px = 300
+lr_crop_px = 550
 images = []
 for i, file in enumerate(filenames):
     image = imageio.imread(d + file)
     print(image.shape, type(image))
-    images.append(image[:, 175:2400])
+    images.append(image[ud_crop_px:-ud_crop_px,
+                        lr_crop_px:-lr_crop_px])
 
 image_sequence = []
 image_sequence.append(images[0])
