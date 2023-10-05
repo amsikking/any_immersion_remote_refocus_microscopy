a4811fe858bd2adc44eef3f13870469d6dd2e376
Alfred Millett-Sikking <amsikking@gmail.com>
2023-10-05 15:45:58 -0700

    Update zoom_lens_gui.py
    Minor update:
    - To simplify the 'main' block the tkinter 'root' is now nested in the 'init'.
    - Arguments for the stage COM ports have been added to ease switching between systems.
    - Improved function with 'verbose' and 'very_verbose' arguments.
    - Improved style with import details and 'return None' where appropriate.

--- figures/zoom_lens/mechanics/zoom_lens_gui.py
+++ figures/zoom_lens/mechanics/zoom_lens_gui.py
@@ -1,21 +1,31 @@
+# Imports from the python standard library:
 import tkinter as tk
 
-import tkinter_compound_widgets as tki_cw # https://github.com/amsikking/tkinter
-import zoom_lens
+# Our code, one .py file per module, copy files to your local directory:
+import tkinter_compound_widgets as tkcw     # github.com/amsikking/tkinter
+import zoom_lens # github.com/amsikking/any_immersion_remote_refocus_microscopy
 
 class GuiZoomLens:
-    def __init__(self, master, name='GuiZoomLens'):
-        self.master = master
+    def __init__(self,
+                 stage1_port,
+                 stage2_port,
+                 stage3_port,
+                 name='GuiZoomLens',
+                 verbose=True,
+                 very_verbose=False):
         self.name = name
-        print('%s: initializing'%self.name)
-        self.zoom_lens = zoom_lens.ZoomLens(
-            stage1_port='COM3',
-            stage2_port='COM5',
-            stage3_port='COM4',
-            verbose=False)
-        frame = tk.LabelFrame(master, text='ZOOM LENS', bd=6)
+        self.verbose = verbose
+        self.root = tk.Tk()
+        self.root.title('Zoom lens GUI')
+        if self.verbose:
+            print('%s: initializing'%self.name)
+        self.zoom_lens = zoom_lens.ZoomLens(stage1_port=stage1_port,
+                                            stage2_port=stage2_port,
+                                            stage3_port=stage3_port,
+                                            verbose=very_verbose)
+        frame = tk.LabelFrame(self.root, text='ZOOM LENS', bd=6)
         frame.grid(row=0, column=0, rowspan=1, padx=20, pady=20, sticky='n')
-        self.sliderspinbox = tki_cw.CheckboxSliderSpinbox(
+        self.sliderspinbox = tkcw.CheckboxSliderSpinbox(
             frame,
             label='focal length (mm)',
             checkbox_enabled=False,
@@ -26,26 +36,35 @@ class GuiZoomLens:
             default_value=self.zoom_lens.f_mm,
             function=self.function)
         quit_button = tk.Button(
-            root, text="QUIT", command=self.quit, height=5, width=30)
+            self.root, text="QUIT", command=self.quit, height=5, width=30)
         quit_button.grid(row=1, column=0, padx=20, pady=20, sticky='n')
-        print('%s: -> done.'%self.name)
-        print('%s: current f_mm   = %s'%(self.name, self.zoom_lens.f_mm))
+        if self.verbose:
+            print('%s: -> done.'%self.name)
+            print('%s: current f_mm   = %s'%(self.name, self.zoom_lens.f_mm))
+        self.root.mainloop()
+        self.root.destroy()
 
     def function(self, slider_value):
-        print('%s: moving to f_mm = %s'%(self.name, slider_value))
+        if self.verbose:
+            print('%s: moving to f_mm = %s'%(self.name, slider_value))
         self.zoom_lens.set_focal_length_mm(slider_value)
-        print('%s: -> done.'%self.name)
+        if self.verbose:
+            print('%s: -> done.'%self.name)
+        return None
 
     def quit(self):
-        print('%s: closing'%self.name)
-        self.zoom_lens.set_focal_length_mm(self.zoom_lens.f_mm_max)
+        if self.verbose:
+            print('%s: closing'%self.name)
         self.zoom_lens.close()
-        self.master.quit()
-        print('%s: -> done.'%self.name)
+        self.root.quit()
+        if self.verbose:
+            print('%s: -> done.'%self.name)
+        return None
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    root.title('Zoom lens GUI')
-    gui_zoom_lens = GuiZoomLens(root)
-    root.mainloop()
-    root.destroy()
+    gui_zoom_lens = GuiZoomLens(
+        'COM3',
+        'COM5',
+        'COM4',
+        verbose=True,
+        very_verbose=False)
